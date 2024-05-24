4f9c7d6862e40192ac597562b5ab31394455448e
Victor Ozoterio <victorozoterio@gmail.com>
2024-05-24 14:39:35 -0300

    feat: listando usuários

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 
 @Controller('/usuarios')
@@ -10,5 +10,9 @@ export class UsuarioController {
     async criaUsuario(@Body()dadosDoUsuario) { // cria um usuário e pega os dados do corpo da requisição (body)
             this.usuarioRepository.salvar(dadosDoUsuario) // salva o usuário criado na memória e passa o dadosDoUsuario como parâmetro
             return dadosDoUsuario; // retorna o corpo da requisição (usuário criado)
+    }
+    @Get()
+    async listaUsuarios() {
+        return this.usuarioRepository.listar();
     }
 }
