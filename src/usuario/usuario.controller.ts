ea4602ba08aa98a4adcf6ae84c6c471507b67e17
Victor Ozoterio <victorozoterio@gmail.com>
2024-05-27 22:39:15 -0300

    feat: validando usuários

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "src/usuario/usuario.repository";
+import { CriaUsuarioDto } from "./dto/criaUsuario.dto";
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -7,7 +8,7 @@ export class UsuarioController {
     constructor(private usuarioRepository: UsuarioRepository) {}
 
     @Post()
-    async criaUsuario(@Body() dadosDoUsuario) { // cria um usuário e pega os dados do corpo da requisição (body)
+    async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDto) { // cria um usuário e pega os dados do corpo da requisição (body)
             this.usuarioRepository.salvar(dadosDoUsuario) // salva o usuário criado na memória e passa o dadosDoUsuario como parâmetro
             return dadosDoUsuario; // retorna o corpo da requisição (usuário criado)
     }
