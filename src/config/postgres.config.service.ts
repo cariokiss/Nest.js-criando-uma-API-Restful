9fc9fb4fd94b429ad806d35e052b0aeeda33b4b2
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-02 22:29:24 -0300

    feat: criando a entidade de produtos no banco de dados

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
-import { UsuarioEntity } from "src/usuario/entity/usuario.entity";
 
 @Injectable()
 export class PostgresConfigService implements TypeOrmOptionsFactory{
@@ -15,7 +14,7 @@ export class PostgresConfigService implements TypeOrmOptionsFactory{
             username: this.configService.get<string>('DB_USERNAME'),
             password: this.configService.get<string>('DB_PASSWORD'),
             database: this.configService.get<string>('DB_NAME'),
-            entities: [UsuarioEntity],
+            entities: [__dirname + '/**/*.entity{.js,.ts}'],
             synchronize: true
         }
     }
--- src/produto/entity/produto.entity.ts
+++ src/produto/entity/produto.entity.ts
@@ -1,21 +1,39 @@
-class CaracteristicaProduto {
-  nome: string;
-  descricao: string;
-}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-class ImagemProduto {
-  url: string;
-  descricao: string;
-}
+// class CaracteristicaProduto {
+//   nome: string;
+//   descricao: string;
+// }
 
+// class ImagemProduto {
+//   url: string;
+//   descricao: string;
+// }
+
+@Entity({ name: 'produtos' })
 export class ProdutoEntity {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'usuario_id', length: 100, nullable: false})
   usuarioId: string;
+  
+  @Column({ name: 'nome', length: 100, nullable: false})
   nome: string;
+
+  @Column({ name: 'valor', nullable: false})
   valor: number;
+  
+  @Column({ name: 'quantidade_disponivel', nullable: false})
   quantidadeDisponivel: number;
+
+  @Column({ name: 'descricao', length: 255, nullable: false})
   descricao: string;
+
+  @Column({ name: 'categoria', length: 100, nullable: false})
   categoria: string;
-  caracteristicas: CaracteristicaProduto[];
-  imagens: ImagemProduto[];
+  
+  // caracteristicas: CaracteristicaProduto[];
+  // imagens: ImagemProduto[];
 }
