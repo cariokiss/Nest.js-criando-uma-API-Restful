87052db4f55ed714aefdfda008912e1b8fcb1436
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-06 09:56:23 -0300

    feat: refatorando os DTOs utilizando o extends

--- src/usuario/dto/atualizaUsuario.dto.ts
+++ src/usuario/dto/atualizaUsuario.dto.ts
@@ -1,17 +1,5 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
-import { EmailEUnico } from '../validacao/email-e-unico.validator';
+import { CriaUsuarioDto } from './criaUsuario.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class AtualizaUsuarioDto {
-  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
-  @IsOptional()
-  nome?: string;
-
-  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
-  @EmailEUnico({ message: 'Já existe um usuário com este e-mail' })
-  @IsOptional()
-  email: string;
-
-  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
-  @IsOptional()
-  senha: string;
-}
+// Aplica o decorator @IsOptional em todas as propriedades do CriaUsuarioDto
+export class AtualizaUsuarioDto extends PartialType(CriaUsuarioDto) {}
